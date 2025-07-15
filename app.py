023a7a86989a18ddd0fdf6fb70e9673334d61d13
Adel Karimov <karrics@MacBook-Air-Adel-5.local>
2025-07-15 15:47:10 +0300

    Version 1.1.1

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ app.secret_key = secrets.token_hex(16)
 
 # Подключение к PostgreSQL
 def get_db_connection():
-    DATABASE_URL = os.environ.get("DATABASE_URL", "postgresql://postgres.arwdrcdztrinbsdcunky:vfnxNhtrth@aws-0-eu-north-1.pooler.supabase.com:6543/postgres")
+    DATABASE_URL = os.environ.get("DATABASE_URL", "postgresql://postgres:vfnxNhtrth@db.arwdrcdztrinbsdcunky.supabase.co:5432/postgres")
     conn = psycopg2.connect(DATABASE_URL)
     return conn
 
